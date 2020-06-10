d86d36f499dffe3de31b86147e5ef22c2f9e1ffc
John Rohan <john.rohan87@gmail.com>
2020-06-10 04:35:46 +0000

    updating finished API working on UI about to begin JS

--- src/api/routes.py
+++ src/api/routes.py
@@ -7,13 +7,32 @@ from api.utils import generate_sitemap
 #from models import Person
 
 api = Blueprint('api', __name__)
+todos = {
+    "task": "homework",
+    "task2": "school",
+    "task3": "hell"
+}
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/get', methods=['GET'])
+def handle_get():
 
-    response_body = {
-        "hello": "world"
-    }
+    response_body = todos
+
+    return jsonify(response_body), 200
+
+@api.route('/post/<new_task>', methods=['POST'])
+def handle_post(new_task):
+
+    tmpdict = todos.update({"new-task":new_task})
+    response_body = tmpdict
+
+    return jsonify(response_body), 200
+
+@api.route('/delete/<key>', methods=['DELETE'])
+def handle_delete(key):
+
+    if key in todos.keys():
+        response_body = todos.pop(key)
 
     return jsonify(response_body), 200
